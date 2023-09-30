d84d1c48922838dcab890c9a3981d0f51bd430b9
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-09-30 23:42:20 +0100

    feat: compress save files

--- src/main/java/io/github/thatsmusic99/hitwtracker/util/GameSaver.java
+++ src/main/java/io/github/thatsmusic99/hitwtracker/util/GameSaver.java
@@ -9,8 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class GameSaver {
 
@@ -28,17 +32,27 @@ public class GameSaver {
 
         // Get the file to save to
         final Date date = statistic.date();
-        final File saveFile = new File(parentFolder, new SimpleDateFormat("dd-MM-yyyy").format(date) + ".json");
+        final String dateFormat = new SimpleDateFormat("dd-MM-yyyy").format(date);
+        final File saveFile = new File(parentFolder, dateFormat + ".json");
+        final File saveZipFile = new File(parentFolder, dateFormat + ".zip");
+
+        final String content;
 
         // If the file doesn't exist, create it
+        if (!saveZipFile.exists()) {
+            if (!saveZipFile.createNewFile()) {
+                throw new IOException("Failed to create file " + saveZipFile.getName());
+            }
+        }
+
         if (!saveFile.exists()) {
             if (!saveFile.createNewFile()) {
                 throw new IOException("Failed to create file " + saveFile.getName());
             }
+            content = getFileContents(saveZipFile);
+        } else {
+            content = getFileContents(saveFile);
         }
-
-        // Read the file
-        String content = getFileContents(saveFile);
 
         // Read as JSON - if all else fails, rename the file and create a new one
         final Gson gson = new GsonBuilder().registerTypeAdapter(Statistic.class, new StatisticSerializer()).create();
@@ -54,23 +68,52 @@ public class GameSaver {
             writer.write(json);
         }
 
-        LOGGER.info("Saved to " + saveFile.getAbsolutePath());
+        try (final ZipOutputStream output = new ZipOutputStream(new FileOutputStream(saveZipFile))) {
+            output.putNextEntry(new ZipEntry(saveFile.getName()));
+            output.write(json.getBytes(StandardCharsets.UTF_8));
+        }
+
+        if (!saveFile.delete()) {
+            LOGGER.warn("Failed to delete " + saveFile.getAbsolutePath());
+        }
+
+        LOGGER.info("Saved to " + saveZipFile.getAbsolutePath());
     }
 
     public static @NotNull String getFileContents(@NotNull File file) throws IOException {
 
-        // Read the file
-        StringBuilder content = new StringBuilder();
-        try (final BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line);
+        // See if it needs unzipping
+        if (file.getName().endsWith(".zip")) {
+            try (ZipInputStream stream = new ZipInputStream(new FileInputStream(file))) {
+                ZipEntry entry = stream.getNextEntry();
+                if (entry != null) {
+                    return read(stream);
+                }
             }
+        }
+
+        try (final InputStream reader = new BufferedInputStream(new FileInputStream(file))) {
+            return read(reader);
+        }
+    }
+
+    private static @NotNull String read(final @NotNull InputStream reader) throws IOException {
+
+        StringBuilder content = new StringBuilder();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = reader.read(buffer)) > 0) {
+            content.append(new String(buffer, 0, length));
         }
 
         // If there's nothing, then
         if (content.isEmpty()) content.append("[]");
 
         return content.toString();
+    }
+
+    private interface NoSuchFileConsumer<T> {
+
+        T get() throws FileNotFoundException;
     }
 }
