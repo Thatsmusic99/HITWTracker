5844e5d8c3081bb68a89dbcdf2daf3782400f64c
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-09-30 23:42:37 +0100

    feat: add less recent damage checkers

--- src/main/java/io/github/thatsmusic99/hitwtracker/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/thatsmusic99/hitwtracker/mixin/LivingEntityMixin.java
@@ -1,20 +1,62 @@
 package io.github.thatsmusic99.hitwtracker.mixin;
 
+import io.github.thatsmusic99.hitwtracker.util.interfaces.LessRecentDamageGetter;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public class LivingEntityMixin implements LessRecentDamageGetter {
 
     @Shadow private long lastDamageTime;
+    @Shadow @Nullable private DamageSource lastDamageSource;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LivingEntityMixin.class);
 
-    @Redirect(method = "getRecentDamageSource", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;lastDamageTime:J", opcode = Opcodes.GETFIELD))
-    private long adjustLastTime(LivingEntity instance) {
+    private long lastDamageTimeAdjusted;
 
-        return lastDamageTime - 60;
+    private @Nullable DamageSource lastDamageSourceAdjusted;
+
+    @Redirect(method = "damage", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;lastDamageTime:J", opcode = Opcodes.PUTFIELD))
+    public void inject(LivingEntity instance, long value) {
+        this.lastDamageTime = value;
+        this.lastDamageTimeAdjusted = System.currentTimeMillis() / 50 / 1000;
+    }
+
+    @Redirect(method = "onDamaged", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;lastDamageTime:J", opcode = Opcodes.PUTFIELD))
+    public void injectDamaged(LivingEntity instance, long value) {
+        this.lastDamageTime = value;
+        this.lastDamageTimeAdjusted = System.currentTimeMillis() / 50 / 1000;
+    }
+
+    @Redirect(method = "damage", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;lastDamageSource:Lnet/minecraft/entity/damage/DamageSource;", opcode = Opcodes.PUTFIELD))
+    public void inject(LivingEntity instance, DamageSource source) {
+        this.lastDamageSource = source;
+        this.lastDamageSourceAdjusted = source;
+    }
+
+    @Redirect(method = "onDamaged", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;lastDamageSource:Lnet/minecraft/entity/damage/DamageSource;", opcode = Opcodes.PUTFIELD))
+    public void injectDamaged(LivingEntity instance, DamageSource source) {
+        this.lastDamageSource = source;
+        this.lastDamageSourceAdjusted = source;
+    }
+
+    public @Nullable DamageSource getLessRecentDamageSource() {
+
+        // Get the time correctly - 1 tick = 50ms
+        long millis = System.currentTimeMillis();
+        long ticks = millis / 50 / 1000;
+        if (ticks - this.lastDamageTimeAdjusted > 80L) {
+            LOGGER.info("Resetting source: " + ticks + ", " + this.lastDamageTimeAdjusted);
+            this.lastDamageSourceAdjusted = null;
+        }
+
+        return this.lastDamageSourceAdjusted;
     }
 }
